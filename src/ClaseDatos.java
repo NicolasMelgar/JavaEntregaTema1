a72d961c52179a63fa1188403b8f511891030992
Nicolas Melgar <elnico521@hotmail.com>
2023-04-06 06:30:14 -0300

    Primer commit Prueba 1 Java OBC

---
+++ src/ClaseDatos.java
@@ -0,0 +1,21 @@
+public class ClaseDatos {
+
+    public static void main(String[] args) {
+        /*
+        Para este primer ejercicio tendréis que realizar lo siguiente:
+    Crea un proyecto de Java desde 0
+    Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+    Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+    Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String.
+         */
+
+        int x = 10;
+        long y = 20;
+        double z = 30.0d;
+        boolean a = true;
+        String b = "Hola";
+
+        System.out.println(b + "(" + x + "+" + y + ")"+ "=" + z);
+        System.out.println("Resultado operación =" + " "+ a);
+    }
+}
